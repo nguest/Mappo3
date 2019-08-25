26b5cc02295c027065ed0f6398c3eff0a7d97bca
nguest <nguest@workwithopal.com>
2019-08-24 17:31:55 -0700

    convert HomeScreen to functional component

--- App/Screens/HomeScreen/index.js
+++ App/Screens/HomeScreen/index.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { useEffect, useState } from 'react';
 import { bool, object } from 'prop-types';
 import { View } from 'react-native';
 import { withNavigationFocus } from 'react-navigation';
@@ -11,106 +11,94 @@ import s from '../../styles';
 import { clearAsyncStorage, saveNewTrack, updateSavedTrack } from '../../helpers/storageManager';
 import { decorateTrack, simplifyPosition, filterPoint } from '../../helpers/pointsManager';
 
-class HomeScreen extends PureComponent {
-  state = {
-    currentPosition: null,
-    currentTrack: [],
-    isRecording: false,
-  }
+const HomeScreen = ({
+  isFocused,
+  navigation,
+}) => {
+  const [currentPosition, setCurrentPosition] = useState(null);
+  const [currentTrack, setCurrentTrack] = useState([]);
+  const [currentTrackId, setCurrentTrackId] = useState(null);
+  const [isRecording, setIsRecording] = useState(false);
 
-  onChangePosition = (position) => {
-    if (!this.props.isFocused) return;
-    if (this.state.isRecording) this.saveTrack(position);
 
-    this.setState((prevState) => {
-      const simplifiedPosition = simplifyPosition(position);
-      let filteredPosition = simplifiedPosition;
-      if (prevState.currentTrack.length) {
-        filteredPosition = filterPoint(
-          simplifiedPosition,
-          prevState.currentTrack[prevState.currentTrack.length - 1],
-        );
-      }
-      const currentTrack = prevState.isRecording && filteredPosition
-        ? [...prevState.currentTrack, filteredPosition]
-        : prevState.currentTrack;
-
-      return {
-        currentPosition: position,
-        currentTrack,
-      };
-    });
-  };
-
-  onCompleteCurrentTrack = () => {
-    console.log('finished!');
-    const { currentTrack, currentTrackId } = this.state;
-    const decoratedTrack = decorateTrack({ track: currentTrack });
-    updateSavedTrack({ id: currentTrackId, track: decoratedTrack });
-    this.props.navigation.navigate('SingleTrack', { track: decoratedTrack });
-    this.setState({ currentTrack: [], currentTrackId: null });
-  }
-
-  onToggleRecord = () => {
-    this.setState((prevState) => {
-      let newTrackId = prevState.currentTrackId;
-      if (!prevState.currentTrackId) {
-        newTrackId = saveNewTrack({
-          initialPosition: simplifyPosition(prevState.currentPosition),
-        });
-      }
-      return {
-        currentTrackId: newTrackId,
-        isRecording: !prevState.isRecording,
-      };
-    });
-  }
-
-  resetTrack = () => {
-    this.setState({
-      currentTrack: [],
-      currentTrackId: null,
-    });
-  }
-
-  saveTrack = (position) => {
-    const { currentTrack, currentTrackId } = this.state;
+  const saveTrack = (position) => {
     if (currentTrack.length % 5 === 0) {
       updateSavedTrack({
         id: currentTrackId,
         track: { id: currentTrackId, data: [...currentTrack, position] },
       });
     }
-  }
+  };
 
-  render() {
+  const onChangePosition = (position) => {
+    if (!isFocused) return;
+    if (isRecording) saveTrack(position);
 
-    //clearAsyncStorage()
-    const { currentPosition, currentTrack, isRecording } = this.state;
-    console.log({ ct: this.state.currentTrack });
+    const simplifiedPosition = simplifyPosition(position);
+    let filteredPosition = simplifiedPosition;
+    if (currentTrack.length) {
+      filteredPosition = filterPoint(
+        simplifiedPosition,
+        currentTrack[currentTrack.length - 1],
+      );
+    }
+    const theCurrentTrack = isRecording && filteredPosition
+      ? [...currentTrack, filteredPosition]
+      : currentTrack;
 
-    return (
-      <View style={[s.align.vCenter, s.align.hCenter]}>
-        <MapView
-          centerCoordinate={currentPosition
-            && [currentPosition.coords.longitude, currentPosition.coords.latitude]}
-          currentTrack={currentTrack}
-          dynamic={true}
-          isRecording={isRecording}
-        />
-        <Dashboard
-          currentPosition={currentPosition}
-          currentTrack={currentTrack}
-          onCompleteCurrentTrack={this.onCompleteCurrentTrack}
-          isRecording={isRecording}
-          onResetTrack={this.resetTrack}
-          onToggleRecord={this.onToggleRecord}
-        />
-        <PositionManagerBG onChangePosition={this.onChangePosition} />
-      </View>
-    );
-  }
-}
+    setCurrentPosition(position);
+    setCurrentTrack(theCurrentTrack);
+  };
+
+  useEffect(() => {
+    if (!isRecording) return;
+    let newTrackId = currentTrackId;
+    if (!currentTrackId) {
+      newTrackId = saveNewTrack({
+        initialPosition: simplifyPosition(currentPosition),
+      });
+    }
+    setCurrentTrackId(newTrackId);
+  }, [isRecording]);
+
+  const resetTrack = () => {
+    setCurrentTrackId(null);
+    setCurrentTrack([]);
+  };
+
+  const onCompleteCurrentTrack = () => {
+    console.log('finished!');
+    const decoratedTrack = decorateTrack({ track: currentTrack });
+    updateSavedTrack({ id: currentTrackId, track: decoratedTrack });
+    navigation.navigate('SingleTrack', { track: decoratedTrack });
+    resetTrack();
+  };
+
+
+  //clearAsyncStorage()
+  console.log({ currentTrack });
+
+  return (
+    <View style={[s.align.vCenter, s.align.hCenter]}>
+      <MapView
+        centerCoordinate={currentPosition
+          && [currentPosition.coords.longitude, currentPosition.coords.latitude]}
+        currentTrack={currentTrack}
+        dynamic={true}
+        isRecording={isRecording}
+      />
+      <Dashboard
+        currentPosition={currentPosition}
+        currentTrack={currentTrack}
+        onCompleteCurrentTrack={onCompleteCurrentTrack}
+        isRecording={isRecording}
+        onResetTrack={resetTrack}
+        onToggleRecord={() => setIsRecording((isRecord) => !isRecord)}
+      />
+      <PositionManagerBG onChangePosition={onChangePosition} />
+    </View>
+  );
+};
 
 HomeScreen.propTypes = {
   isFocused: bool.isRequired,
